80a33a12c191095bc979c00d0647f7a5c0056a34
AungKoThet2k7 <aungkothat.mgn19@gmail.com>
2025-11-27 09:34:41 +0630

    template login & dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth')->group(function () {
+    Route::get('dashboard', fn() => view('dashboard'))->name('dashboard');
+    Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 });
